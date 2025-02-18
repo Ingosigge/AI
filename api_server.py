96572e54f7b2bb19888e19eb09332c3d16bb1f3d
Ingosigge <mikael@nygrenholding.se>
2025-02-18 19:33:24 +0100

    Update api_server.py

--- api_server.py
+++ api_server.py
@@ -46,5 +46,8 @@ def get_trends():
     
     return jsonify(df_news.to_dict(orient='records'))
 
+import os
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
